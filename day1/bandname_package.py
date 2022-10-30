737b234d424892af017b70255c54695b60af7824
Mikelbernard12 <michaelmbungai@gmail.com>
2022-10-30 17:00:15 +0100

    initial commit

---
+++ day1/bandname_package.py
@@ -0,0 +1,6 @@
+print('welcome to the band Name Generator\n'
+      'what is your pet name ? : ')
+name = input()
+print('what is the name of your city ?:')
+city_name = input()
+print('Your band name is '+ name + ' ' + city_name)
