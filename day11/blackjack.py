03d1a782a14d5922c87f7fc2f03916bd08c0c18f
Michael bernard <97235186+Mikelbernard12@users.noreply.github.com>
2022-12-05 13:04:36 +0100

    Add files via upload
    completed version of the project

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -1,34 +1,70 @@
 import random
-should_continue = True
-while should_continue:
+from replit import clear
+
+
+def deal_card():
+    """  returns a number from the deck"""
     numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    users = random.choice(numbers)
-    s_users = random.choice(numbers)
-    user = [users, s_users]
-    print(f"user = {user}")
-    computer1 = random.choice(numbers)
-    computer2 = random.choice(numbers)
-    computer = [computer1, computer2]
-    print(f"computer = {computer}")
-    a = users + s_users
-    b = computer1 + computer2
-    if b == 21 and a == 21:
-        print("The computer has a blackjack , computer wins")
-    elif a == 21:
-        print("The user has a blackjack , user wins")
-    elif b == 21:
-        print("The computer has a blackjack , computer wins")
-    elif a > 21:
-        for value in user:
-            val = value
-            if val == 11:
-                num = user.index(val)
-                user[num] = 1
-        print("The user is burst,the user losses")
-    res = input("user, do you want another card 'y' for yes or 'n' for no : ")
-    if res == 'y':
-        user1 = random.choice(numbers)
-        a += user1
-    if b <= 16:
-        computer3 = random.choice(numbers)
-        b += computer3
+    card = random.choice(numbers)
+    return card
+
+
+def calc_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "it's a draw"
+    elif computer_score == 0 :
+        return "you lose,the computer has a blackjack"
+    elif user_score == 0 :
+        return "you win,you have a blackjack"
+    elif user_score > 21:
+        return "you lose,the computer has won"
+    elif computer_score > 21:
+        return "you win"
+    elif user_score > computer_score:
+        return "you win"
+    else:
+        return "you lose,the computer has won"
+
+
+def play_game():
+    s_users = []
+    computer = []
+    is_game_over = False
+    for number in range(2):
+        s_users.append(deal_card())
+        computer.append(deal_card())
+    while not is_game_over:
+        user_score = calc_score(s_users)
+        computer_score = calc_score(computer)
+        print(f" your score :{s_users}, current score : {user_score}")
+        print(f" computer's score :{computer[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_continue = input("type 'y' o continue or 'n' to stop: ").lower()
+            if user_should_continue == 'y':
+                s_users.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score < 17 and computer_score !=0:
+        computer.append(deal_card())
+        computer_score = calc_score(computer)
+    print(f"your score :{s_users}, final score : {user_score}")
+    print(f"computer's score :{computer},final score : {computer_score}")
+    print(compare(user_score, computer_score))
+
+
+while input("do ou want to play blackjack,type 'y' for yes or 'n' for no : ").lower() == 'y':
+    clear()
+    play_game()
